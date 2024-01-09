ee7849da7ced02017e8fa00dc197aa0d179c458d
unknown <usern5949@gmail.com>
2024-01-09 12:55:02 +0530

    Added components, functional and class components

---
+++ hello-world/src/components/Greet.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const Greet = () => {
+    return ( 
+        <h1>Hello Vishwas</h1>
+     );
+}
+// Arrow functions are preferred way of declaring functions.
+export default Greet;
