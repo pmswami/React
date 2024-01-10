f6c9cdb68991a541b3d8e8c88b4872064de51692
unknown <usern5949@gmail.com>
2024-01-10 16:04:28 +0530

    Added styling

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -5,16 +5,24 @@ import EventBind from './components/EventBind';
 import FunctionClick from './components/FunctionClick';
 import Greet from './components/Greet';
 import Hello from './components/Hello';
+import Inline from './components/Inline';
 import Message from './components/Message';
 import NameList from './components/NameList';
 import ParentComponent from './components/ParentComponent';
+import Stylesheet from './components/Stylesheet';
 import UserGreeting from './components/UserGreeting';
 import Welcome from './components/Welcome';
+import './appStyles.css'
+import styles from './appStyles.module.css'
 
 function App() {
   return (
     <div className="App">
-      <NameList/>
+      <h1 className="error">Error</h1>
+      <h1 className={styles.success}>Success</h1>
+      {/* <Inline/> */}
+      {/* <Stylesheet primary={true}/> */}
+      {/* <NameList/> */}
       {/* <UserGreeting/> */}
       {/* <ParentComponent/> */}
       {/* <EventBind/> */}
