6f6c2bc93f42a945d1d824280eeb646bd4c97009
unknown <usern5949@gmail.com>
2024-01-12 12:30:30 +0530

    Added Errors and error boundaries

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -26,12 +26,23 @@ import Input from './components/Input';
 // import FocusInput from './components/FocusInput';
 import FRParentInput from './components/FRParentInput';
 import PortalDemo from './components/PortalDemo';
+import Hero from './components/Hero';
+import ErrorBoundary from './components/ErrorBoundary';
 
 
 function App() {
   return (
     <div className="App">
-      <PortalDemo/>
+      <ErrorBoundary>
+        <Hero heroName="Batman"/>
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <Hero heroName="Superman"/>
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <Hero heroName="Joker"/>
+      </ErrorBoundary>
+      {/* <PortalDemo/> */}
       {/* <FRParentInput/> */}
       {/* <FocusInput/> */}
       {/* <Input/> */}
