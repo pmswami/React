626979dd1254404f3d37f1941b80a9771f23b6c2
unknown <usern5949@gmail.com>
2024-01-12 23:55:01 +0530

    Added useState with arrays

--- state-hook/src/App.js
+++ state-hook/src/App.js
@@ -3,11 +3,13 @@ import ClassCounter from "./components/ClassCounter";
 import HookCounter from "./components/HookCounter";
 import HookCounter2 from "./components/HookCounter2";
 import HookCounter3 from "./components/HookCounter3";
+import HookCounter4 from "./components/HookCounter4";
 
 function App() {
   return (
     <div className="App">
-      <HookCounter3/>
+      <HookCounter4/>
+      {/* <HookCounter3/> */}
       {/* <HookCounter2/> */}
       {/* <ClassCounter/> */}
       {/* <HookCounter/> */}
