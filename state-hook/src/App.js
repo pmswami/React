cfb995335719b5a04652bedcdc756d7447a25ff0
unknown <usern5949@gmail.com>
2024-01-14 00:14:49 +0530

    Added conditionally running useEffect

--- state-hook/src/App.js
+++ state-hook/src/App.js
@@ -1,5 +1,7 @@
 import "./App.css"
 import ClassCounter from "./components/ClassCounter";
+import ClassCounter1 from "./components/ClassCounter1";
+import DemoUseEffect from "./components/DemoUseEffect";
 import HookCounter from "./components/HookCounter";
 import HookCounter2 from "./components/HookCounter2";
 import HookCounter3 from "./components/HookCounter3";
@@ -8,7 +10,9 @@ import HookCounter4 from "./components/HookCounter4";
 function App() {
   return (
     <div className="App">
-      <HookCounter4/>
+      <DemoUseEffect/>
+      {/* <ClassCounter1/> */}
+      {/* <HookCounter4/> */}
       {/* <HookCounter3/> */}
       {/* <HookCounter2/> */}
       {/* <ClassCounter/> */}
