7a3c8e2891096e27fd35b01b533ddad6269eebb5
unknown <usern5949@gmail.com>
2024-01-29 18:34:34 +0530

    Added useCounter custom hook

--- custom-hook/src/App.js
+++ custom-hook/src/App.js
@@ -1,13 +1,17 @@
 
 import './App.css';
+import Counter1 from './components/Counter1';
+import Counter2 from './components/Counter2';
 import DocTitle1 from './components/DocTitle1';
 import DocTitle2 from './components/DocTitle2';
 
 function App() {
   return (
     <div className="App">
-     <DocTitle1/>
-     <DocTitle2/>
+     {/* <DocTitle1/> */}
+     {/* <DocTitle2/> */}
+     <Counter1/>
+     <Counter2/>
     </div>
   );
 }
---
+++ custom-hook/src/components/Counter2.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import useCounter from '../hooks/useCounter'
+
+function Counter2() {
+    // const [count, setCount] = useState(0)
+
+    // const increment = () =>{
+    //     setCount(prevCount => prevCount+1)
+    // }
+
+    // const decrement = () =>{
+    //     setCount(prevCount => prevCount-1)
+    // }
+
+    // const reset = () =>{
+    //     setCount(0)
+    // }
+
+    const [count, increment, decrement, reset] = useCounter()
+    
+  return (
+    <div>
+        <h2>Count - {count}</h2>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={reset}>Reset</button>
+    </div>
+  )
+}
+
+export default Counter2
