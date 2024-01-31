f619014a43a5d3770b431038251b40170687c6be
unknown <usern5949@gmail.com>
2024-02-01 01:34:20 +0530

    Added rendering in useReducer hook

--- react-render/src/App.js
+++ react-render/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import { UseReducer } from './components/UseReducer/UseReducer';
 import { UseState } from './components/UseState/UseState';
 
 function App() {
   return (
     <div className="App">
-      <UseState/>
+      {/* <UseState/> */}
+      <UseReducer/>
     </div>
   );
 }
---
+++ react-render/src/components/UseReducer/UseReducer.js
@@ -0,0 +1,29 @@
+import React, { useReducer } from 'react'
+
+const initialState = 0
+
+const reducer = (state, action) => {
+    switch(action){
+        case "increment":
+            return state + 1
+        case "decrement":
+            return state - 1
+        case "reset":
+            return initialState
+        default:
+            return state
+    }
+}
+
+export const UseReducer = () => {
+    const [count, dispatch] = useReducer(reducer, initialState)
+    console.log("usereducer render")
+  return (
+    <div>
+        <h1>Count - {count}</h1>
+        <button onClick={()=>dispatch("increment")}>Increment</button>
+        <button onClick={()=>dispatch("decrement")}>Decrement</button>
+        <button onClick={()=>dispatch("reset")}>Reset</button>
+    </div>
+  )
+}
